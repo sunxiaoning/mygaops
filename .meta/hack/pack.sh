70e706e563f2f94e2c6bddb367696ea5ae8483d9
sunxiaoning <xiaoning.sun@huolala.cn>
2024-09-09 21:53:19 +0800

    Support mygaops ops.

---
+++ .meta/hack/pack.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+set -o pipefail
+
+. .meta/hack/env.sh
+
+EXCLUDES=(
+  --exclude='*/README.md'
+  --exclude='*/.meta'
+  --exclude='*/.gh_token.txt'
+  --exclude='*/.git'
+  --exclude='*/.git*'
+  --exclude='*/.dmypasswd.txt'
+)
+
+mkdir -p "${PKG_PATH}"
+
+gtar "${EXCLUDES[@]}" -czvf "${PKG_PATH}/${PKG_NAME}" .
+
---
+++ .meta/hack/release.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+set -o pipefail
+
+. .meta/hack/env.sh
+
+trap cleanup EXIT
+
+CLEAN_DONE=0
+cleanup() {
+  if [[ ${CLEAN_DONE} -eq 1 ]]; then
+      return
+  fi
+  CLEAN_DONE=1
+  echo "Received signal EXIT, performing cleanup..."
+
+  rm -rf "${PKG_PATH}"
+
+  echo "Cleanup done."
+}
+
+REL_TAG="v${PKG_VERSION}"
+REL_TITLE="Release v${PKG_VERSION}"
+REL_NOTES="Init release."
+
+install-rel() {
+  if gh release view "${REL_TAG}" &>/dev/null; then
+    echo "Release ${REL_TAG} already exists!"
+    return
+  fi
+
+  #gh release create "${REL_TAG}" "${PKG_PATH}/${PKG_NAME}" --title "${REL_TITLE}" --notes "${REL_NOTES}"
+}
+
+auth-gh() {
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/sunxiaoning/ghcli/main/autorun.sh)"
+}
+
+main() {
+  auth-gh
+  install-rel
+}
+
+main "$@"
