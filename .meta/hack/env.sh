9a75970dedcc989bcc100381c7c9dcb6e79722f1
sunxiaoning <xiaoning.sun@huolala.cn>
2024-09-13 21:43:38 +0800

    Fix pkg_name rule.

--- .meta/hack/env.sh
+++ .meta/hack/env.sh
@@ -5,9 +5,10 @@ set -o pipefail
 USE_DOCKER=${USE_DOCKER:-"0"}
 
 PKG_PATH=".meta/hack/pack"
-PKG_NAME="mygaops.tar.gz"
 
 PKG_VERSION="1.0.3"
+PKG_NAME="mygaops-${PKG_VERSION}.tar.gz"
+
 
 REL_TAG="v${PKG_VERSION}"
 REL_TITLE="Release v${PKG_VERSION}"
