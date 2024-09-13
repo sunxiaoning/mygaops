eebbf909dde1aa2a59163b3f9ff5690a934ce03b
sunxiaoning <xiaoning.sun@huolala.cn>
2024-09-13 22:31:22 +0800

    Update release note.

--- .meta/hack/env.sh
+++ .meta/hack/env.sh
@@ -6,11 +6,11 @@ USE_DOCKER=${USE_DOCKER:-"0"}
 
 PKG_PATH=".meta/hack/pack"
 
-PKG_VERSION="1.0.3"
+PKG_VERSION="1.0.4"
 PKG_NAME="mygaops-${PKG_VERSION}.tar.gz"
 
 
 REL_TAG="v${PKG_VERSION}"
 REL_TITLE="Release v${PKG_VERSION}"
-REL_NOTES="Optimize logic for checking Galera Cluster status."
+REL_NOTES="Optimize pkg_name rule."
 
