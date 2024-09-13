2acb7ce4ebfa4ee31a8f6d924871404264c288c3
sunxiaoning <xiaoning.sun@huolala.cn>
2024-09-13 14:05:36 +0800

    Update Release Note.

--- .meta/hack/env.sh
+++ .meta/hack/env.sh
@@ -7,9 +7,9 @@ USE_DOCKER=${USE_DOCKER:-"0"}
 PKG_PATH=".meta/hack/pack"
 PKG_NAME="mygaops.tar.gz"
 
-PKG_VERSION="1.0.2"
+PKG_VERSION="1.0.3"
 
 REL_TAG="v${PKG_VERSION}"
 REL_TITLE="Release v${PKG_VERSION}"
-REL_NOTES="Update bashutils."
+REL_NOTES="Optimize logic for checking Galera Cluster status."
 
