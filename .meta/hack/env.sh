da2146f8ffbf000517b0cbcaccb4dffab956550b
sunxiaoning <1530358579@qq.com>
2024-09-21 17:14:03 +0800

    docs: fix formatting and clarity in release notes

--- .meta/hack/env.sh
+++ .meta/hack/env.sh
@@ -13,12 +13,12 @@ REL_TAG="v${PKG_VERSION}"
 REL_TITLE="Release v${PKG_VERSION}"
 REL_NOTES="
 ## Improvements
-- Optimized the \'init\' function to ensure idempotent operations.
-- Improved the \'reinit\' function to prevent unsafe operations.
-- Refined the \'stop\' function to include \'reset-failed\' before execution.
-- Enhanced the \'WSREP_CLUSTER_ADDRESS_ARRAY\' environment variable to maintain an idempotent sequence.
+- Optimized the \`init\` function to ensure idempotent operations.
+- Improved the \`reinit\` function to prevent unsafe operations.
+- Refined the \`stop\` function to include \`reset-failed\` before execution.
+- Enhanced the \`WSREP_CLUSTER_ADDRESS_ARRAY\` environment variable to maintain an idempotent sequence.
 - Refined various check logic to ensure function robustness.
 
 ## Bug Fixes
-- Fixed an issue with the \'execrsh.sh\' script where the \'SSH_OPTIONS\' environment variable was not applied correctly.
+- Fixed an issue with the \`execrsh.sh\` script where the \`SSH_OPTIONS\` environment variable was not applied correctly.
 "
