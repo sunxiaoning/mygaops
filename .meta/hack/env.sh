157ed06da3ecefde49c134b34a2b0372fbeb06d8
sunxiaoning <xiaoning.sun@huolala.cn>
2024-09-30 21:06:35 +0800

    feat: remove autoboot target and update README.md
    - removed the `autoboot` target.
    - updated the `README.md`.

--- .meta/hack/env.sh
+++ .meta/hack/env.sh
@@ -6,13 +6,13 @@ USE_DOCKER=${USE_DOCKER:-"0"}
 
 PKG_PATH=".meta/hack/pack"
 
-PKG_VERSION="1.1.9"
+PKG_VERSION="1.1.10"
 PKG_NAME="mygaops-${PKG_VERSION}.tar.gz"
 
 REL_TAG="v${PKG_VERSION}"
 REL_TITLE="Release v${PKG_VERSION}"
 REL_NOTES="
 ## Improvements
-- Optimized the \`start\` operation logic.
-- Updated the \`bashutils\` submodule.
+- Removed the \`autoboot\` target.
+- Updated the \`README.md\`.
 "
