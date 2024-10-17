0b0f9d08598f498cf5167a84d287e33e7daacc43
sunxiaoning <1530358579@qq.com>
2024-10-17 23:52:25 +0800

    feat: optimize the mygaops operations process
    - refined the `install`,`run` `uninstall` logic for improved efficiency and clarity.
    - updated the `bashutils` and `rpmrepo` submodule versions.

--- ops/install.sh
+++ ops/install.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE}")")
-
-. ops/env.sh
-
-RPMREPO_MODULE=rpmrepo
+RPMREPO_SH_FILE="${CONTEXT_DIR}/rpmrepo/rpmrepo.sh"
 
 export REPO_SOURCE=${REPO_SOURCE:-"0"}
 export REPO_SERVER_PROTOCOL=${REPO_SERVER_PROTOCOL:-"http"}
@@ -17,38 +12,54 @@ export MYSQLD_INNODB_BUFFER_POOL_SIZE=${MYSQLD_INNODB_BUFFER_POOL_SIZE:-122M}
 export MYSQLD_WSREP_PROVIDER=${MYSQLD_WSREP_PROVIDER:-/usr/lib64/galera-4/libgalera_smm.so}
 export MYSQLD_MYSQLD_WSREP_PROVIDER_OPTIONS=${MYSQLD_MYSQLD_WSREP_PROVIDER_OPTIONS:-gcache.size=300M; gcache.page_size=300M}
 export MYSQLD_WSREP_CLUSTER_NAME=${MYSQLD_WSREP_CLUSTER_NAME:-galera-cluster}
-export MYSQLD_WSREP_CLUSTER_ADDRESS=${MYSQLD_WSREP_CLUSTER_ADDRESS:-""}
+export MYSQLD_WSREP_CLUSTER_ADDRESS
 export MYSQLD_WSREP_NODE_NAME=${MYSQLD_WSREP_NODE_NAME:-$(hostname)}
-export MYSQLD_WSREP_NODE_ADDRESS=${MYSQLD_WSREP_NODE_ADDRESS:-$(hostname -I | awk '{print $1}')}
+export MYSQLD_WSREP_NODE_ADDRESS
 
 STOP_SERV_ON_INSTALL=${STOP_SERV_ON_INSTALL:-"0"}
 
-PROJECT_PATH=$(pwd)
-
-YUMINSTALLER_SH_FILE="${SCRIPT_DIR}/../bashutils/yuminstaller.sh"
-RENDER_SH_FILE="${SCRIPT_DIR}/../bashutils/render.sh"
-CHECKHOSTIP_SH_FILE="${SCRIPT_DIR}/../bashutils/checkhostip.sh"
+RENDER_SH_FILE="${CONTEXT_DIR}/bashutils/render.sh"
 
 install-repo() {
-  cd "${RPMREPO_MODULE}"
-
-  hack/build.sh galera4
-
-  hack/install.sh galera4
-
-  yum clean all &>/dev/null
-  yum makecache
-  cd "${PROJECT_PATH}"
+  "${RPMREPO_SH_FILE}" install-galera4
 }
 
 install-app() {
-  if rpm -q "${GALERA_NAME}-${GALERA_VERSION}" &>/dev/null && rpm -q "${MYSQL_WSREP_NAME}-${MYSQL_WSREP_VERSION}" &>/dev/null; then
-    echo "Galera4 cluster already installed!"
+  install-galera-rpm
+  install-mysql-wsrep-rpm
+}
+
+install-galera-rpm() {
+  if rpm -q "${GALERA_NAME}-${GALERA_VERSION}" &>/dev/null; then
+    echo "Galera RPM is already installed!"
     return 0
   fi
 
+  check-mysqld-stopped
+
+  dnf -y module disable mysql mariadb >/dev/null
+
+  echo "Install ${GALERA_NAME}-${GALERA_VERSION} ..."
+  "${YUMINSTALLER_SH_FILE}" -o "-y" "${GALERA_NAME}" "${GALERA_VERSION}"
+}
+
+install-mysql-wsrep-rpm() {
+  if rpm -q "${MYSQL_WSREP_NAME}-${MYSQL_WSREP_VERSION}" &>/dev/null; then
+    echo "MySQL RPM is already installed!"
+    return 0
+  fi
+
+  check-mysqld-stopped
+
+  dnf -y module disable mysql mariadb >/dev/null
+
+  echo "Install ${MYSQL_WSREP_NAME}-${MYSQL_WSREP_VERSION} ..."
+  "${YUMINSTALLER_SH_FILE}" -o "-y" "${MYSQL_WSREP_NAME}" "${MYSQL_WSREP_VERSION}"
+}
+
+check-mysqld-stopped() {
   if [[ "1" == "${STOP_SERV_ON_INSTALL}" ]]; then
-    ops/run.sh stop
+    stop
   else
     local service_status=$(systemctl is-active mysqld 2>/dev/null || true)
     if [[ "${service_status}" != "inactive" ]] && [[ "${service_status}" != "dead" ]]; then
@@ -56,18 +67,11 @@ install-app() {
       exit 1
     fi
   fi
-
-  dnf -y module disable mysql mariadb &>/dev/null
-
-  echo "Install ${GALERA_NAME}-${GALERA_VERSION} ..."
-  "${YUMINSTALLER_SH_FILE}" -o "-y" "${GALERA_NAME}" "${GALERA_VERSION}"
-
-  echo "Install ${MYSQL_WSREP_NAME}-${MYSQL_WSREP_VERSION} ..."
-  "${YUMINSTALLER_SH_FILE}" -o "-y" "${MYSQL_WSREP_NAME}" "${MYSQL_WSREP_VERSION}"
 }
 
 install-conf() {
   check-clusteraddress
+
   if ! rpm -q "${GALERA_NAME}-${GALERA_VERSION}" &>/dev/null; then
     echo "${GALERA_NAME}-${GALERA_VERSION} has not been installed yet!"
     exit 1
@@ -78,88 +82,9 @@ install-conf() {
     exit 1
   fi
 
-  "${RENDER_SH_FILE}" conf/my.cnf.tmpl "conf/my.cnf"
-  trap 'rm -rf "conf/my.cnf"' EXIT
+  "${RENDER_SH_FILE}" "${CONTEXT_DIR}/conf/my.cnf.tmpl" "${CONTEXT_DIR}/conf/my.cnf"
 
-  install -D -m 644 "conf/my.cnf" "/etc/my.cnf"
+  TEMP_FILES+=("${CONTEXT_DIR}/conf/my.cnf")
+
+  install -D -m 644 "${CONTEXT_DIR}/conf/my.cnf" "/etc/my.cnf"
 }
-
-check-clusteraddress() {
-  if [ -z "${MYSQLD_WSREP_CLUSTER_ADDRESS}" ]; then
-    echo "MYSQLD_WSREP_CLUSTER_ADDRESS is empty!" >&2
-    exit 1
-  fi
-
-  IFS=',' read -r -a WSREP_CLUSTER_ADDRESS_ARRAY <<<"${MYSQLD_WSREP_CLUSTER_ADDRESS}"
-
-  if [ "${#WSREP_CLUSTER_ADDRESS_ARRAY[@]}" -lt 2 ]; then
-    echo "MYSQLD_WSREP_CLUSTER_ADDRESS is invalid!" >&2
-    exit 1
-  fi
-
-  WSREP_CLUSTER_ADDRESS_ARRAY=($(printf "%s\n" "${WSREP_CLUSTER_ADDRESS_ARRAY[@]}" | LC_ALL=C sort -s -t '.' -k1,1n -k2,2n -k3,3n -k4,4n))
-
-  local check_hostip_error=$("${CHECKHOSTIP_SH_FILE}" "${MYSQLD_WSREP_NODE_ADDRESS}" 2>&1 || true)
-  if [ ! -z "${check_hostip_error}" ]; then
-    echo "MYSQLD_WSREP_NODE_ADDRESS: ${MYSQLD_WSREP_NODE_ADDRESS} not matched any host_ip!" >&2
-    exit 1
-  fi
-
-  MYSQLD_WSREP_NODE_ADDRESS_INDEX="-1"
-  for index in "${!WSREP_CLUSTER_ADDRESS_ARRAY[@]}"; do
-    if [ "${WSREP_CLUSTER_ADDRESS_ARRAY[$index]}" != "${MYSQLD_WSREP_NODE_ADDRESS}" ]; then
-      continue
-    fi
-
-    MYSQLD_WSREP_NODE_ADDRESS_INDEX="$index"
-    break
-
-  done
-
-  if [[ ${MYSQLD_WSREP_NODE_ADDRESS_INDEX} == "-1" ]]; then
-    echo "MYSQLD_WSREP_NODE_ADDRESS: ${MYSQLD_WSREP_NODE_ADDRESS} not matched any MYSQLD_WSREP_CLUSTER_ADDRESS: ${MYSQLD_WSREP_CLUSTER_ADDRESS} !" >&2
-    exit 1
-  fi
-}
-
-main() {
-  if [[ "1" == "${USE_DOCKER}" ]]; then
-    echo "Begin to build with docker."
-    case "${1-}" in
-    repo)
-      install-repo-docker
-      ;;
-    app)
-      install-app-docker
-      ;;
-    conf)
-      install-conf-docker
-      ;;
-    *)
-      install-repo-docker
-      install-app-docker
-      install-conf-docker
-      ;;
-    esac
-  else
-    echo "Begin to build in the local environment."
-    case "${1-}" in
-    repo)
-      install-repo
-      ;;
-    app)
-      install-app
-      ;;
-    conf)
-      install-conf
-      ;;
-    *)
-      install-repo
-      install-app
-      install-conf
-      ;;
-    esac
-  fi
-}
-
-main "$@"
--- ops/uninstall.sh
+++ ops/uninstall.sh
@@ -1,65 +1,65 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(dirname "$(realpath "${BASH_SOURCE}")")
-
-. ops/env.sh
-
 CLEAN_DATA_ON_UNINSTALL=${CLEAN_DATA_ON_UNINSTALL:-"0"}
 STOP_SERV_ON_UNINSTALL=${STOP_SERV_ON_UNINSTALL:-"0"}
 
 uninstall-app() {
-  if [[ "1" == "${STOP_SERV_ON_UNINSTALL}" ]]; then
-    ops/run.sh stop
-  else
-    local service_status=$(systemctl is-active mysqld 2>/dev/null || true)
-    if [[ "${service_status}" != "inactive" ]] && [[ "${service_status}" != "dead" ]]; then
-      echo "Service mysqld has not been shutdown completed!" >&2
-      exit 1
-    fi
+  uninstall-galera-rpm
+
+  uninstall-mysql-wsrep-rpm
+
+  clean-mysql-datadir
+}
+
+uninstall-galera-rpm() {
+  if ! rpm -q "${GALERA_NAME}-${GALERA_VERSION}" &>/dev/null; then
+    echo "Galera RPM is not exists, skip the operation!"
+    return 0
   fi
+
+  check-mysqld-stopped
 
   # TODO check-safebootstrap, the last commit node should be the last node to be uninstalled.
 
   yum -y remove "${GALERA_NAME}-${GALERA_VERSION}"
+
   if rpm -q "${GALERA_NAME}-${GALERA_VERSION}" &>/dev/null; then
-    echo "Remove Galera4 failed!" >&2
+    echo "Remove Galera RPM failed!" >&2
     exit 1
   fi
+}
+
+uninstall-mysql-wsrep-rpm() {
+  if ! rpm -q "${MYSQL_WSREP_NAME}-${MYSQL_WSREP_VERSION}" &>/dev/null; then
+    echo "MySQL-WSREP RPM is not exists, skip the operation!"
+    return 0
+  fi
+
+  check-mysqld-stopped
+
+  # TODO check-safebootstrap, the last commit node should be the last node to be uninstalled.
 
   yum -y remove "${MYSQL_WSREP_NAME}-${MYSQL_WSREP_VERSION}"
+
   if rpm -q "${MYSQL_WSREP_NAME}-${MYSQL_WSREP_VERSION}" &>/dev/null; then
-    echo "Remove Mysql_Wserp failed!" >&2
+    echo "Remove MySQL-WSREP RPM failed!" >&2
     exit 1
   fi
+}
 
+check-mysqld-stopped() {
+  if [[ "1" == "${STOP_SERV_ON_UNINSTALL}" ]]; then
+    stop
+  else
+    local service_status=$(systemctl is-active mysqld 2>/dev/null || true)
+    if [[ "${service_status}" != "inactive" ]] && [[ "${service_status}" != "dead" ]]; then
+      echo "Service mysqld has not been shutdown completed!"
+      exit 1
+    fi
+  fi
+}
+
+clean-mysql-datadir() {
   if [[ "1" == "${CLEAN_DATA_ON_UNINSTALL}" ]]; then
     echo "[Warning] Clean old MySQL datadir ..."
     rm -rf ${MYSQLD_DATADIR}
   fi
 }
-
-main() {
-  if [[ "1" == "${USE_DOCKER}" ]]; then
-    echo "Begin to build with docker."
-    case "${1-}" in
-    app)
-      uninstall-app-docker
-      ;;
-    *)
-      uninstall-app-docker
-      ;;
-    esac
-  else
-    echo "Begin to build in the local environment."
-    case "${1-}" in
-    app)
-      uninstall-app
-      ;;
-    *)
-      uninstall-app
-      ;;
-    esac
-  fi
-}
-
-main "$@"
